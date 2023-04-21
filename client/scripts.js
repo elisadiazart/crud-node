1773a0ff5c4b3ddcfe3234fe290a27d66e68a3b6
Eugenia Valls <elisadiazartcontact@gmail.com>
2023-04-21 21:42:34 +0200

    21.04.23

--- client/scripts.js
+++ client/scripts.js
@@ -1,10 +1,12 @@
+
+
 const main = document.getElementById('main')
+const createUserButton = document.getElementById('createUserButton')
+const formCreateUser = document.getElementById('formCreateUser')
 
-
-const fetchData = async (url, method) => {
-    const response = await fetch(url);
+const fetchData = async (url, ...options) => {
+    const response = await fetch(url, ...options);
     const data = await response.json();
-    console.log(data);
     return data;
     
   };
@@ -24,27 +26,90 @@ const printUsers = async () => {
         deleteButton.textContent = 'Borrar'
         deleteButton.dataset.userId = user.userId
         deleteButton.dataset.type = 'delete-button'
-        userContainer.append(userName, button,deleteButton)
+        const editButton = document.createElement('button')
+        editButton.textContent= 'Editar'
+        editButton.dataset.type = 'edit-button'
+        userContainer.append(userName, button,deleteButton, editButton)
+        userContainer.id = user.userId
         fragment.append(userContainer)
     })
     main.append(fragment)
 }
 
-const printUserDetails = async (button) => {
-    const data = await fetchData(`http://localhost:3000/api/users/${button.dataset.userId}`);
+const printUserDetails = async (userId) => {
+    const data = await fetchData(`http://localhost:3000/api/users/${userId}`, {
+        method:'GET',
+    });
     const userEmail = document.createElement('p');
     userEmail.textContent = data.email;
-    button.parentElement.append(userEmail);
+    document.getElementById(userId).append(userEmail);
 }
 
-const deleteUserDetails = async (button,) => {
-    const data = await fetchData(`http://localhost:3000/api/users/${button.dataset.userId}`,'DELETE')
+const deleteUserDetails = async (userId) => {
+    const data = await fetchData(`http://localhost:3000/api/users/${userId}`, {
+        method:'DELETE',
+    })
+}
+
+const printSearchInput = (container) => {
+    const form = document.createElement('form')
+    const searchInput = document.createElement('input')
+    const name = container.firstChild
+    searchInput.placeholder = name.textContent
+    const submitButton = document.createElement('input')
+    form.dataset.userId = name.nextSibling.dataset.userId
+    form.id='form'
+    submitButton.type ='submit'
+    submitButton.dataset.type ='submit'
+    form.append(searchInput, submitButton)
+    container.append(form)
+    
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        changeName(form.dataset.userId,searchInput.value )
+        name.textContent == searchInput.value
+    })
+}
+
+const changeName = async (userId, newName)  => {            
+    const data = await fetchData(`http://localhost:3000/api/users/${userId}`, {
+        method:'PATCH',
+        body: JSON.stringify({name: newName}),
+        headers: {
+            Accept: 'application/json',
+
+            'Content-Type': 'application/json'
+}
+    })
+}
+
+const createUser = async (form) => {
+    console.log(form.name.value);
+    const data = await fetchData(`http://localhost:3000/api/users/`, {
+        method:'POST',
+        body: JSON.stringify({name: form.name.value,
+        email: form.email.value
+    }),
+        headers: {
+            Accept: 'application/json',
+
+            'Content-Type': 'application/json'
+}
+    })
 }
 
 document.body.addEventListener('click', (e) => {
-    if(e.target.dataset.type === 'button') printUserDetails(e.target)
-    if(e.target.dataset.type === 'delete-button') deleteUserDetails(e.target)
+    if(e.target.dataset.type === 'button') printUserDetails(e.target.dataset.userId)
+    if(e.target.dataset.type === 'delete-button') deleteUserDetails(e.target.dataset.userId)
+    if(e.target.dataset.type === 'edit-button') printSearchInput(e.target.parentElement)
     
 })
+
+formCreateUser.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createUser(e.target)
+})
+
+
 
 printUsers()
--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -7,7 +7,7 @@ const controller = {}
 
 controller.allUsers = (req, res) => {
     fs.readFile(usersFile, (err, data) => {
-        if (err) res.status(500).send('Error al leer el archivo de usurios')
+        if (err) res.status(500).send({message:'Error al leer el archivo de usurios'})
 
         const jsonData = JSON.parse(data)
 
@@ -17,7 +17,7 @@ controller.allUsers = (req, res) => {
 
 controller.userById = (req, res) => {
     fs.readFile(usersFile, (err, data) => {
-        if (err) res.status(500).send('Error al leer el archivo de usurios')
+        if (err) res.status(500).send({message:'Error al leer el archivo de usurios'})
         const jsonData = JSON.parse(data)
         const user = jsonData.find(user =>{
             return user.userId === req.params.id})
@@ -35,7 +35,7 @@ controller.createUser = (req, res) => {
         const userCheck = jsonData.some(user => user.email === newData.email)
 
         if(userCheck){
-            return res.status(409).send('Usuario Duplicado')
+            return res.status(409).send({message:'Usuario Duplicado'})
         }
 
         newData.userId = v4()
@@ -43,7 +43,7 @@ controller.createUser = (req, res) => {
         jsonData.push(newData)
 
     fs.writeFile(usersFile, JSON.stringify(jsonData), err=> {
-        if(err) return res.status(500).send('Error al guardar el usuario')
+        if(err) return res.status(500).send({message:'Error al guardar el usuario'})
         res.end()
     })
 })}
@@ -51,20 +51,20 @@ controller.createUser = (req, res) => {
 controller.deleteUser = (req, res) => {
     fs.readFile('./data/users.json', (err, data)=> {
         if (err) throw err;
-        const newData = req.params.userId
+        const newData = req.params.id
 
         const jsonData = JSON.parse(data)
 
         const userIndex = jsonData.findIndex(user => user.userId === newData)
         
         if(userIndex === -1){
-            return res.status(409).send('Ese usuario no existe')
+            return res.status(409).send({message:'Ese usuario no existe'})
         }
 
         jsonData.splice(userIndex, 1)
 
         fs.writeFile(usersFile, JSON.stringify(jsonData), err=> {
-            if(err) return res.status(500).send('Error al guardar el usuario')
+            if(err) return res.status(500).send({message:'Error al guardar el usuario'})
             res.end()
         })
 
@@ -81,7 +81,7 @@ controller.patchUser = (req, res) => {
         const userPosition = jsonData.findIndex(user => user.userId === userId)
 
         if(userPosition===-1){
-            return res.status(404).send('Ese usuario no existe')
+            return res.status(404).send({message:'Ese usuario no existe'})
         }
 
         if(req.body.email){
@@ -89,7 +89,7 @@ controller.patchUser = (req, res) => {
             const userCheck = jsonData.some(user => user.email === req.body.email)
 
             if(userCheck){
-                return res.status(409).send('No se ha podido actualizar el email porque ya esta en uso')
+                return res.status(409).send({message:'No se ha podido actualizar el email porque ya esta en uso'})
             }
         }
 
@@ -100,8 +100,8 @@ controller.patchUser = (req, res) => {
         jsonData[userPosition] = userPatched
         
         fs.writeFile(usersFile, JSON.stringify(jsonData), err=> {
-            if(err) return res.status(500).send('Error al guardar el usuario')
-            return res.status(200).send('Usuario actualizado')
+            if(err) return res.status(500).send({message:'Error al guardar el usuario'})
+            return res.status(200).send({message:'Usuario actualizado'})
         })
 
     })
